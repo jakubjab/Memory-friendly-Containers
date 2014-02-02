904c44a06d978c61825af9c5edf84a99d739181e
Jakub Jabłoński <jakubjab@data.pl>
2014-02-02 20:49:08 +0100

    Second commit

---
+++ memoryfriendlycontainers/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  memoryfriendlycontainers
+//
+//  Created by Jakub Jabłoński on 02.02.2014.
+//  Copyright (c) 2014 Jakub Jabłoński. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
